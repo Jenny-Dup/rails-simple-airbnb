d392d04e9bddd7d85d3cc9cb0c77c972bd8dea3e
Jennifer Du Preez <jennydupreez13@gmail.com>
2024-05-21 20:27:55 +0100

    final code added for simple airbnb challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+
+Flat.create!(
+  name: 'Tiny Flat London',
+  address: '15 Firth Gardens London SW9 1PT',
+  description: 'A quaint little flat. Suitable for couple only. Has nice TV and shower',
+  price_per_night: 50,
+  number_of_guests: 2
+  picture_url: 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+)
+
+Flat.create!(
+  name: 'Beautiful House In London',
+  address: '6 Blueberry Road London NW9 3ZX',
+  description: 'Gorgeous, large family home in London. Three double bedrooms, open plan living area, large kitchen and a beautiful garden',
+  price_per_night: 120,
+  number_of_guests: 6
+  picture_url: 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+)
+
+Flat.create!(
+  name: 'Grand Country House',
+  address: '111 Sunset Avenue SW11 6BB',
+  description: 'A fantastic country house to escape to. Six grand double bedrooms with ensuite, open plan living area, swimming pool and tennis court',
+  price_per_night: 300,
+  number_of_guests: 12
+  picture_url: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+)
